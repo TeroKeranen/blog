e5a60f96394baa91e94e110f26a1d9dacc21a59e
Tero Keränen <tero-keranen@hotmail.com>
2023-01-04 13:34:41 +0200

    functions to funtions.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 //
 // import functions
-const {getDate, changpass, findBlog, findBlogById, findByIdAndDelete} = require("./functions.js");
+const {getDate, changpass, findBlog, findBlogById, findByIdAndDelete, makeNewBlogPost, postComment, registerFunc, logIn} = require("./functions.js");
 //  require mongoose model
 const User = require('./models/user');
 const { render } = require('ejs');
@@ -94,6 +94,9 @@ app.delete('/home/:id', (req,res) => {
 app.get("/registeredAbout", (req,res) => {
     res.render('registeredAbout', {title: "about"})
 })
+//
+//
+// Go to registered index when logged in
 app.get("/registeredIndex", (req,res) => {
     res.render("registeredIndex", {title: "home"} )
 })
@@ -103,75 +106,25 @@ app.get("/login", (req,res) => {
     
     res.render('login', {title: "Login"});
 })
-
-
+//
+//
 // Post new blog to home page when you are logged in
 app.post('/home', (req,res) => {
     
-    const userId = req.user.id;
-    const numberOfPosts = req.user.posts;
-
-    // Making new blog that include your username that you are logged in so we can display it to home page
-    const blog = new Blog({
-        id: req.user.id, //Annetaan tietokannalle sama id kuin mikä käyttäjän id on.
-        username: req.user.username,
-        title: req.body.title,
-        body: req.body.body,
-        date: getDate()
-
-        
-    })
-    
-    blog.save()
-        .then((result) => {
-            res.redirect('/home')
-        })
-        .catch((err) => console.log(err));
-    
-
-    User.findById(userId, (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            result.set({
-                posts: numberOfPosts + 1
-            })
-        }
-        result.save((err) => {
-            console.log(err);
-
-        })
-    })
-
+    // check funtion info at functions.js
+    makeNewBlogPost(req,res);
 
 })
-
+//
+//
+// Post a comment
 app.post("/home/:id", (req,res) => {
-    const user = req.user.username; // Get username
-    const userComment = req.body.comment // Get comment that user post
-    const id = req.params.id;
-    const commentDate = getDate(); // get date when comment posted
 
-    console.log(commentDate);
-
-    
-
-   
-    // Add comment to database with username 
-    Blog.updateOne({_id:id},{$push: {comments: {comment: userComment, userCom: user,commetDate: commentDate}}}, (err) => {
-         if(err) {
-             console.log(err)
-         } else {
-             res.redirect(`${id}`);
-         }
-     } )
-    
-
-
+    // Function that make commenting possible, check more information at functions.js
+    postComment(req,res);
     
 })
-
-
+//
 //
 // when user go to new blog page it render to this create page
 app.get("/blogs/create", (req,res) => {
@@ -182,17 +135,19 @@ app.get("/image", (req,res) => {
     res.render('image', {title: "Valitse kuva"})
 })
 //
+//
 // render register page
-
-
 app.get("/register", (req,res) => {
     
     res.render('register', {title: 'Register'});
 })
-
+//
+//
+// render to changepassword page
 app.get("/changepassword", (req,res) => {
     res.render('changepassword', {title: 'change password'})
 })
+//
 //
 // when you log in you will render to secret page
 app.get('/secret', (req,res) => {
@@ -207,6 +162,7 @@ app.get('/secret', (req,res) => {
         res.redirect('/login');
     }
 })
+//
 //
 // Logging out
 app.get('/logout', (req,res) => {
@@ -223,63 +179,17 @@ app.post("/changepassword", (req,res) => {
 // Register new user
 app.post("/register", (req,res) => {
 
-    number = 0 // Add post number to database;
-    const passwordInput = req.body.password; // get first password input
-    const passwordInput2 = req.body.password2; // get second password input
-    const userInput = req.body.username; // get username input
-    
-    const msg = "Salasanat eivät olleet samoja"; // display this when passwords is not same
-    const usermsg = "Käyttäjänimi on jo käytössä"; // display this if username is taken
-    
-
-    // Try find entered username value in the User database, if user is already in database display error message on the page
-    User.findOne({username: userInput}, function(err, foundUser) {
-
-        
-        // This tells us that user is already in database
-        if(foundUser) {
-
-            res.render('register', {usererr: usermsg, title: "Register"})
-        
-        // If username not found in database it will continue
-        } else {
-
-            // if password and password2 is not the same then error message will display
-            if (passwordInput === passwordInput2) {
-
-                User.register({username: req.body.username, posts: number, registeredDate: getDate()}, req.body.password, function(err, user) {
-                
-                    if(err) {
-                        console.log(err)
-                        res.redirect('/register')
-        
-                    } else {
-            
-                        passport.authenticate('local')(req,res,function() {
-                        res.redirect('/secret');
-                        })
-                    }
-                })
-            
-            // if passwords is not the same it will render back to register page and display a error message
-            } else {
-                res.render('register', {errmsg: msg, title: "Register"})
-        
-            }
-
-        }
-    })        
+    registerFunc(req,res);        
 })
 //
 //
 //
+// Go to user information page
 app.get("/omatTiedot", (req,res) => {
 
-    const userName = req.user.username;
-    const numberOfPosts = req.user.posts;
-    const registeredDate = req.user.registeredDate;
-    
-    
+    const userName = req.user.username; // get ur username
+    const numberOfPosts = req.user.posts; // get info how many post you made
+    const registeredDate = req.user.registeredDate;  // Get info when you registered your username
     
     
     res.render("omatTiedot", {title: "Omat tiedot",userName,numberOfPosts, registeredDate})
@@ -290,30 +200,7 @@ app.get("/omatTiedot", (req,res) => {
 app.post("/login", (req,res) => {
     
     
-    // Make a new user object when logging in
-    const user = new User({
-        
-        username:req.body.username,
-        password:req.body.password
-    })
-    //
-    // check if login informations is correct
-    req.login(user, (err) => {
-        if(err) {
-            console.log(err)
-            
-        } else {
-            
-            
-            passport.authenticate('local') (req,res, function () {
-                
-                
-
-                res.redirect('/home')
-
-            })
-        }
-    } )
+   logIn(req,res);
     
 })
 //
--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 const User = require('./models/user');
 const Blog = require('./models/blog');
+const session = require('express-session');
+const passport = require('passport') 
 
 // Get current date function
 function getDate ()  {
@@ -78,11 +80,152 @@ function findByIdAndDelete (req,res) {
 
 }
 
+// app.post('/home' this function make a new blog post, save the blog, find ur username in the database and update ur posted blogs info
+
+function makeNewBlogPost (req,res) {
+
+    const userId = req.user.id;
+    const numberOfPosts = req.user.posts;
+
+    // Making new blog that include your username that you are logged in so we can display it to home page
+    const blog = new Blog({
+        id: req.user.id, //Annetaan tietokannalle sama id kuin mikä käyttäjän id on.
+        username: req.user.username,
+        title: req.body.title,
+        body: req.body.body,
+        date: getDate()
+
+        
+    })
+    
+    // save a new blog post
+    blog.save()
+        .then((result) => {
+            res.redirect('/home')
+        })
+        .catch((err) => console.log(err));
+    
+    // find you user in the database and update posted blogs number
+    User.findById(userId, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            result.set({
+                posts: numberOfPosts + 1
+            })
+        }
+        result.save((err) => {
+            console.log(err);
+
+        })
+    })
+}
+
+
+// app.post("/home/:id" Post a comment to blogs
+function postComment (req,res) {
+
+    const user = req.user.username; // Get username
+    const userComment = req.body.comment // Get comment that user post
+    const id = req.params.id;
+    const commentDate = getDate(); // get date when comment posted
+
+    // Add comment to database with username 
+    Blog.updateOne({_id:id},{$push: {comments: {comment: userComment, userCom: user,commetDate: commentDate}}}, (err) => {
+         if(err) {
+             console.log(err)
+         } else {
+             res.redirect(`${id}`);
+         }
+     } )
+}
+
+function registerFunc (req,res) {
+    number = 0 // Add post number to database;
+    const passwordInput = req.body.password; // get first password input
+    const passwordInput2 = req.body.password2; // get second password input
+    const userInput = req.body.username; // get username input
+    
+    const msg = "Salasanat eivät olleet samoja"; // display this when passwords is not same
+    const usermsg = "Käyttäjänimi on jo käytössä"; // display this if username is taken
+    
+
+    // Try find entered username value in the User database, if user is already in database display error message on the page
+    User.findOne({username: userInput}, function(err, foundUser) {
+
+        
+        // This tells us that user is already in database
+        if(foundUser) {
+
+            res.render('register', {usererr: usermsg, title: "Register"})
+        
+        // If username not found in database it will continue
+        } else {
+
+            // if password and password2 is not the same then error message will display
+            if (passwordInput === passwordInput2) {
+
+                User.register({username: req.body.username, posts: number, registeredDate: getDate()}, req.body.password, function(err, user) {
+                
+                    if(err) {
+                        console.log(err)
+                        res.redirect('/register')
+        
+                    } else {
+            
+                        passport.authenticate('local')(req,res,function() {
+                        res.redirect('/secret');
+                        })
+                    }
+                })
+            
+            // if passwords is not the same it will render back to register page and display a error message
+            } else {
+                res.render('register', {errmsg: msg, title: "Register"})
+        
+            }
+
+        }
+    })        
+}
+
+// Login function 
+function logIn (req,res) {
+    // Make a new user object when logging in
+    const user = new User({
+        
+        username:req.body.username,
+        password:req.body.password
+    })
+    //
+    // check if login informations is correct
+    req.login(user, (err) => {
+        if(err) {
+            console.log(err)
+            
+        } else {
+            
+            
+            passport.authenticate('local') (req,res, function () {
+                
+                
+
+                res.redirect('/home')
+
+            })
+        }
+    } )
+}
+
 module.exports = {
     getDate,
     changpass,
     findBlog,
     findBlogById,
     findByIdAndDelete,
+    makeNewBlogPost,
+    postComment,
+    registerFunc,
+    logIn,
 }
 
